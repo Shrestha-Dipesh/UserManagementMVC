78106327e38ccda7226d1fce3bf1cbe0838a2e03
shrestha-dipesh <shrestha.dipesh1999@gmailcom>
2023-03-20 12:22:52 +0545

    Implemented functionality to add new user along with server and client side validations

--- UserManagementWebApp/Controllers/UserController.cs
+++ UserManagementWebApp/Controllers/UserController.cs
@@ -1,19 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UserManagementWebApp.Data;
+using UserManagementWebApp.Models;
 
 namespace UserManagementWebApp.Controllers
 {
     public class UserController : Controller
     {
         private readonly ApplicationDbContext _context;
+
         public UserController(ApplicationDbContext context)
         {
             _context = context;
         }
+
         public IActionResult Index()
         {
             var users = _context.Users.ToList();
             return View(users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User user)
+        {
+            if (!ModelState.IsValid)
+                return View(user);
+
+            if (user.Gender == null)
+                user.Gender = "N/A";
+
+            if (user.Address == null)
+                user.Address = "N/A";
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- UserManagementWebApp/Models/User.cs
+++ UserManagementWebApp/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace UserManagementWebApp.Models
 {
@@ -7,9 +8,12 @@ namespace UserManagementWebApp.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
         [Required]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+        [DisplayName("Date of Birth")]
         public DateTime DateOfBirth { get; set; }
         public string? Gender { get; set; }
         public string? Address { get; set; }
