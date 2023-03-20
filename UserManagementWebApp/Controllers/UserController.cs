58db4a48275d472e2d141352b1ae164cd91e3552
shrestha-dipesh <shrestha.dipesh1999@gmailcom>
2023-03-20 09:52:00 +0545

    Added controller and view to display the list of users

---
+++ UserManagementWebApp/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagementWebApp.Data;
+
+namespace UserManagementWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+    }
+}
